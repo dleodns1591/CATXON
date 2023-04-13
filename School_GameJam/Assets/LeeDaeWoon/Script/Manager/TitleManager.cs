e1dc125769b1830a8098866a3e3f7cbdc87cad23
dleodns1591 <88140817+dleodns1591@users.noreply.github.com>
2023-04-13 16:47:11 +0900

    타이틀 수정
    타이틀 수정

--- School_GameJam/Assets/LeeDaeWoon/Script/Manager/TitleManager.cs
+++ School_GameJam/Assets/LeeDaeWoon/Script/Manager/TitleManager.cs
@@ -8,11 +8,6 @@ public class TitleManager : MonoBehaviour
 {
     [Header("타이틀 버튼")]
     [SerializeField] Button startBtn;
-    [SerializeField] Button creditBtn;
-    [SerializeField] Button mainBtn;
-
-    [Header("화면")]
-    [SerializeField] GameObject creditWindow;
 
     void Start()
     {
@@ -30,18 +25,6 @@ public class TitleManager : MonoBehaviour
         {
             SoundManager.Instance.audioSources[0].Play();
             SceneManager.LoadScene("Ingame");
-        });
-
-        creditBtn.onClick.AddListener(() =>
-        {
-            SoundManager.Instance.audioSources[0].Play();
-            creditWindow.SetActive(true);
-        });
-
-        mainBtn.onClick.AddListener(() =>
-        {
-            SoundManager.Instance.audioSources[0].Play();
-            creditWindow.SetActive(false);
         });
     }
 }
